2174a6d5525eae5e1a8eb2f06a749a8a9628164e
Antonio3veces3 <cramirez28@ucol.mx>
2022-08-04 18:02:56 -0500

    Pruebas TDD con JEST

---
+++ index.js
@@ -0,0 +1,15 @@
+const Matriz = require("./models/matriz");
+//FUNCION PARA PRUEBA
+
+let matriz = new Matriz(8, 4);
+matriz.crearMatriz();
+matriz.llenarMatriz();
+let celulasVivas = [
+  [1, 4],
+  [2, 3],
+  [2, 4],
+];
+matriz.fillLiveCells(celulasVivas);
+console.log("________________");
+matriz.imprimirMatriz(matriz.siguienteGeneracion());
+
